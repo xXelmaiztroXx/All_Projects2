02df5293e24180b2634bdf6b6af31db1bf2eb81e
Angel Torres <angel.torres07@utrgv.edu>
2022-04-17 15:25:22 -0500

    Add game project
    A game development exercise using Unity.

---
+++ Games/Demo Game/Assets/coision.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (Equals(c.GetComponent<Collider>().tag, "wall"))
+        {
+            // We hit a wall...
+            // Take damage...
+            // Restart the level...
+        }
+        
+    }
+}
